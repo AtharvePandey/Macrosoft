56c77e77a869bded58865e4d423f866f5f5b880d
ProphecyOak <drito610@gmail.com>
2025-03-14 10:01:45 -0400

    Fixed @ sign alias
    You can now reference the different highest folders using `@asset/`, `@component/, `@page/`, and `@style`

--- Polispace/src/pages/root.tsx
+++ Polispace/src/pages/root.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "@/styles/index.css";
-import App from "@/pages/App.tsx";
+import "@style/index.css";
+import App from "@page/App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
