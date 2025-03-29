e59253f71608b1cca6e192654986da1080c44a43
Adam Smith <adamjsmith002@gmail.com>
2025-03-29 12:26:06 -0400

    setup for different pages

--- Polispace/src/pages/App.tsx
+++ Polispace/src/pages/App.tsx
@@ -1,32 +1,37 @@
-import { useState } from "react";
-import reactLogo from "../assets/react.svg";
 import "../styles/App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Comment from "../components/Comment";
+import Home from "./Home";
+import Settings from "./Settings";
+import CreatePost from "./CreatePost";
 
 function App() {
-  const [count, setCount] = useState(0);
+  //const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <Comment />
-    </>
+    <Router>
+      <>
+        <h1>Polispace app</h1>
+        <nav>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/Settings">Settings</a>
+          </li>
+          <li>
+            <a href="/CreatePost">Create a Post</a>
+          </li>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/createpost" element={<CreatePost />} />
+        </Routes>
+        <Comment />
+      </>
+    </Router>
   );
 }
 
---
+++ Polispace/src/pages/CreatePost.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CreatePost = () => {
+  return (
+    <div>
+      <h2>Create a post here</h2>
+    </div>
+  );
+};
+
+export default CreatePost;
