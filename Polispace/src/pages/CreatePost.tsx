450cfc12deef2937f35172ccfae4f6b85d98258c
Adam Smith <adamjsmith002@gmail.com>
2025-03-30 12:00:49 -0400

    more changes

--- Polispace/src/pages/CreatePost.tsx
+++ Polispace/src/pages/CreatePost.tsx
@@ -3,18 +3,20 @@ import React from "react";
 const CreatePost = () => {
   return (
     <div className="bg-green-500">
-      <h2>Create a post here</h2>
-      <nav>
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/Settings">Settings</a>
-        </li>
-        <li>
-          <a href="/CreatePost">Create a Post</a>
-        </li>
-      </nav>
+      <div className="bg-green-500 min-h-screen mx-4">
+        <h1 className="mx-4">Create a post here</h1>
+        <nav>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/Settings">Settings</a>
+          </li>
+          <li>
+            <a href="/CreatePost">Create a Post</a>
+          </li>
+        </nav>
+      </div>
     </div>
   );
 };
--- Polispace/src/pages/Settings.tsx
+++ Polispace/src/pages/Settings.tsx
@@ -3,18 +3,20 @@ import React from "react";
 const Settings = () => {
   return (
     <div className="bg-blue-600">
-      <h2>Welcome to the Settings Page</h2>
-      <nav>
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/Settings">Settings</a>
-        </li>
-        <li>
-          <a href="/CreatePost">Create a Post</a>
-        </li>
-      </nav>
+      <div className="bg-blue-600 min-h-screen mx-4">
+        <h1 className="mx-4">Welcome to the Settings Page</h1>
+        <nav>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/Settings">Settings</a>
+          </li>
+          <li>
+            <a href="/CreatePost">Create a Post</a>
+          </li>
+        </nav>
+      </div>
     </div>
   );
 };
