c27bd4f893aaadbcd72b86ea3dc11ffe5e835217
ProphecyOak <drito610@gmail.com>
2025-04-04 18:48:52 -0400

    Feed sort of working
    Manipulated DataWrapper to have state hold across files. Made the components generally utilize the DataWrapper

--- Polispace/src/datawrapper.ts
+++ Polispace/src/datawrapper.ts
@@ -1,65 +1,177 @@
- 
-import { PostInterface, UserInterface, PostClassification, CommentInterface } from "./datamodels";
+import {
+  PostInterface,
+  UserInterface,
+  PostClassification,
+  CommentInterface,
+  FeedInterface,
+  FeedScope,
+  ReactionInterface,
+  PartialReturn,
+} from "./datamodels";
 import { Role } from "./datamodels";
 
 //we need a datawrapper that has mock data of a user
+const userData: Map<string, UserInterface> = new Map([
+  [
+    "TEST1",
+    {
+      userId: "1",
+      username: "Alice",
+      email: "alice@example.com",
+      passwordHash: "hashed_pw",
+      role: Role.Poster,
+      feeds: {
+        [FeedScope.Local]: "TEST",
+      },
+    },
+  ],
+  [
+    "TEST2",
+    {
+      userId: "2",
+      username: "Bob",
+      email: "bob@example.com",
+      passwordHash: "hashed_pw",
+      role: Role.Commenter,
+      feeds: {
+        [FeedScope.Local]: "TEST",
+      },
+    },
+  ],
+]);
 
-export const DataWrapper = (() => {
-    //this wrapper will have a map which stores key user ID and val can be userinterface type
-    const userData: Map<string, UserInterface> = new Map([
-        ["1", { userId: "1", username: "Alice", email: "alice@example.com", passwordHash: "hashed_pw", role: Role.Poster, feeds: {} }],
-        ["2", { userId: "2", username: "Bob", email: "bob@example.com", passwordHash: "hashed_pw", role: Role.Commenter, feeds: {} }],
-    ]);
+//mock feed
+const feedData: Map<string, FeedInterface> = new Map([
+  [
+    "TEST",
+    {
+      feedID: "TEST",
+      feedLevel: FeedScope.Local,
+      regionID: "Washington, D.C.",
+      getPosts(_start: string, _amount: number) {
+        return [
+          {
+            item: postData.get("TEST")!,
+            id: "TEST",
+            fetch() {
+              return this.item;
+            },
+          } as PartialReturn<PostInterface>,
+          {
+            item: postData.get("TEST2")!,
+            id: "TEST2",
+            fetch() {
+              return this.item;
+            },
+          } as PartialReturn<PostInterface>,
+        ];
+      },
+    },
+  ],
+]);
 
-    //gonna do the same thing for a mock post obj
+//gonna do the same thing for a mock post obj
+const postData: Map<string, PostInterface> = new Map([
+  [
+    "TEST",
+    {
+      postId: "1",
+      authorId: "2",
+      title: "some title here",
+      contentType: PostClassification.Poll,
+      primaryContent: "skdlsjf",
+      textContent: "poll : who is better, ur mom or ur mom",
+      category: "policy",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      getComments(_start, _amount) {
+        return [
+          {
+            item: commentData.get("TEST")!,
+            id: "TEST",
+            fetch() {
+              return this.item;
+            },
+          } as PartialReturn<CommentInterface>,
+        ];
+      },
+      getReactionAmounts() {
+        return new Map<ReactionInterface, number>();
+      },
+      getReactions() {
+        return [];
+      },
+    },
+  ],
+  [
+    "TEST2",
+    {
+      postId: "2",
+      authorId: "1",
+      title: "some title here",
+      contentType: PostClassification.Text,
+      primaryContent: "skdlsjf",
+      textContent: "your face",
+      category: "policy",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      getComments(_start, _amount) {
+        return [];
+      },
+      getReactionAmounts() {
+        return new Map<ReactionInterface, number>();
+      },
+      getReactions() {
+        return [];
+      },
+    },
+  ],
+]);
 
-    const postData: Map<string, PostInterface> = new Map([
-        ["1", {
-            postId: "1",
-            authorId: "2",
-            title: "some title here",
-            contentType: PostClassification,
-            primaryContent: "skdlsjf",
-            textContent: "poll : who is better, ur mom or ur mom",
-            category: "policy",
-            createdAt: Date.now(),
-            updatedAt: Date.now(),
-            //idk how to add the 3 call back function we defined
-        }]
-    ])
-    //same thing for comments, where post ID for comments will match post
+//same thing for comments, where post ID for comments will match post
+const commentData: Map<string, CommentInterface> = new Map([
+  [
+    "TEST",
+    {
+      commentId: "1",
+      postId: "1",
+      authorId: "1", //user 1 commented on user 2's post
+      content: "lol f u",
+      createdAt: new Date(), //using this to mock stuff
+      updatedAt: new Date(),
+    },
+  ],
+]);
 
-    const commentData: Map<string, CommentInterface> = new Map([
-        [
-            "1", {
-                commentId: "1",
-                postId: "1",
-                authorId: "1", //user 1 commented on user 2's post
-                content: "lol f u",
-                createdAt: Date.now(), //using this to mock stuff
-                updatedAt: Date.now()
-            }
-        ]
-    ]);
+async function getFromCollection<T>(
+  collection: Map<string, T>,
+  id: string
+): Promise<T | undefined> {
+  return new Promise((resolve) =>
+    setTimeout(() => resolve(collection.get(id)), 300)
+  );
+}
 
-    return {
-        async getUser(userId: string): Promise<UserInterface | undefined> {
-            return new Promise(resolve => setTimeout(() => resolve(userData.get(userId)), 300));
-        },
-        async getPost(postId: string): Promise<PostInterface | undefined> {
-            return new Promise(resolve => setTimeout(() => resolve(postData.get(postId)), 300));
-        },
-        async getComments(postId: string): Promise<CommentInterface[]> {
-            return new Promise(resolve => setTimeout(() => resolve(Array.from(commentData.values()).filter((c: CommentInterface) => c.postId === postId)), 300));
-        },
-    };
-})();
+const DataWrapper = (() => ({
+  getCurrentUser: () => getFromCollection(userData, "TEST1")!,
+  getFeed: (feedId: string) => getFromCollection(feedData, feedId),
+  getUser: (userId: string) => getFromCollection(userData, userId),
+  getPost: (postId: string) => getFromCollection(postData, postId),
+  async getComments(postId: string): Promise<CommentInterface[]> {
+    // THIS SHOULD BE CHANGED TO GRAB FROM LIST OF COMMENT IDS IN POST DATASTRUCTURE
 
-
-
+    return new Promise((resolve) =>
+      setTimeout(
+        () =>
+          resolve(
+            Array.from(commentData.values()).filter(
+              (c: CommentInterface) => c.postId === postId
+            )
+          ),
+        300
+      )
+    );
+  },
+}))();
 
 export default DataWrapper;
-
-
-
-
