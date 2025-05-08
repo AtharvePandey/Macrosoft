b8eacdea438498d98d1029e2fdea019362ccd923
Atharve Pandey <pandey.atharve@gmail.com>
2025-05-08 14:58:36 -0400

    added logic to get user, similar logic will follow for CRUDDING the rest of the db

--- Polispace/server/server.ts
+++ Polispace/server/server.ts
@@ -1,12 +1,27 @@
 import express, { Request, Response } from 'express';
-import {DataWrapper} from '../src/datawrapper';
+import cors from "cors";
+import DataWrapper from '../src/datawrapper';
+
+const dataWrapper: typeof DataWrapper = DataWrapper;
 
 const app: express.Application = express();
 
 const port = 3001; //making this port cause idk what other parts of the applications use
 
-app.get('/', (req:Request, res:Response) => {
-    
+app.use(cors());
+
+// /user/atharve --> where atharve will be the user we are searching for
+
+app.get('/user/:user', (req:Request, res:Response) => {
+    const {user} = req.body;
+    if(!user){
+        res.status(404).send("user is null, try querying again"); //user not found? actually this should be done in the physical datawrapper class...
+    }
+    try{
+        dataWrapper.getUser(req.body);
+    }catch{
+        res.status(404).send("user not in the db");
+    }
 });
 
 
